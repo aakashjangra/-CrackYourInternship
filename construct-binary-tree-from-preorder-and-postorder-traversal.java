42bdffaf2dc02bd99858e44f76c0681a79d0ff93
Akash <aj.akashjangra@gmail.com>
2024-08-09 22:56:18 +0530

    construct-binary-tree-from-preorder-and-postorder-traversal.java

---
+++ construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,34 @@
+// Problem link - https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// TC - O(n)
+// SC - O(n)
+class Solution {
+    int preIndex = 0, posIndex = 0;
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        TreeNode root = new TreeNode(pre[preIndex++]);
+        if(root.val != post[posIndex]){
+            root.left = constructFromPrePost(pre, post);
+        }
+        if(root.val != post[posIndex]){
+            root.right = constructFromPrePost(pre, post);
+        }
+        posIndex++;
+        return root;
+    }
+}
