de26684fb2f79f418cc2e68ecb895212f72c1464
Akash <aj.akashjangra@gmail.com>
2024-08-20 20:43:51 +0530

    longest-repeating-character-replacement.java

---
+++ longest-repeating-character-replacement.java
@@ -0,0 +1,32 @@
+// Problem link - https://leetcode.com/problems/longest-repeating-character-replacement/
+
+// TC - O(N)
+// SC - O(1)
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n = s.length();
+        int i = 0;
+        int j = 0; 
+        int maxC = 0;
+        int ans = 0;
+        int []freq = new int[26];
+        
+        while(j < n){
+            char charj = s.charAt(j);
+            freq[charj-'A']++;
+            maxC = Math.max(maxC, freq[charj-'A']);
+            int curLen = j - i + 1;
+            
+            if(curLen - maxC > k){
+                char chari = s.charAt(i);
+                freq[chari-'A']--;
+                i++;
+            }
+            curLen = j - i + 1;
+            ans = Math.max(ans, curLen);
+            j++;
+        }
+        
+        return ans;
+    }
+}
